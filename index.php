189db71b279c71bdb52c49fb68d7e738c433ea2c
Itallo Vidal <48143354+ividalll666@users.noreply.github.com>
2020-04-28 22:48:07 -0300

    Primeiro Artigo <3

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+
+	<title> Nutricionando </title>
+</head>
+
+<body class="interface">
+
+
+	<figure class="foto-inicial">
+		<img src="_imagens/perfil.jpg">
+	</figure>
+
+	<nav>
+		<ul class="menu">
+			<li> Inicio</li>
+			<li> Matérias</li>
+			<li> Sobre nós </li>
+
+
+		</ul>
+	</nav>
+ 
+
+
+	<section class="conteudo">
+		
+			<h1> Por que não emagreço? </h1>
+
+			<p> 
+				&nbsp &nbsp Você provavelmente já fez ou conhece alguém que teve a seguinte dúvida: <b>"Por que eu não consigo emagrecer?"</b>. Apesar de seguir a dieta como planejado, não tem o <b> resultado </b>esperado no <b> tempo </b> esperado. Então, hoje,  eu estarei aqui pra desmistificar alguns aspectos relacionados ao emagrecimento.
+
+			</p>
+
+			<h2> O que é emagrecer? </h2>
+
+			<p>
+
+			&nbsp &nbsp O emagrecimento não está relacionado ao seu peso corporal, e sim a sua <i> massa gorda </i>, ou em termos mais simples, a gordura que está estocada no seu corpo. Emagrecer é diminuir a quantidade de massa gorda que seu corpo possui. O ato de se pesar depois de um treino intenso e relatar um peso menor do que antes é muito comum entre os praticantes de atividade física, entretanto, lamento informar que aquele peso menor não significa que você perdeu tudo aquilo de gordura. É claro que um pouco de gordura foi queimada, mas um quantidade ínfima. A maior parte do peso perdido após um treino intenso é devido a perda de água no suor, com a função de equilibrar a sua temperatura corporal durante e após o treino. Além disso, é importante ressaltar que não importa o quanto você treine para emagrecer, se sua dieta não estiver com os valores adequados, você não irá ter o resultado esperado. 
+
+			</p>
+
+			
+			<div class="destaque">
+				<p>
+
+				"A maior parte do peso perdido após um treino intenso é devido a perda de água no suor(...)"
+
+				</p>
+
+			</div>
+
+			<h2> Déficit, manutenção e superávit calórico simplificados </h2>
+
+			<p>	
+
+			&nbsp &nbsp Imagine que você recebe R$3000 por semana e seus custos totalizam R$2000. No final do mês, você terá R$1000 de reserva.	Se você passasse a gastar R$3500, você teria uma dívida de R$500 a cada mês, certo?	A reserva neste exemplo, faz analogia à sua <b> massa gorda </b>: sua reserva de gordura. Para uma pessoa emagrecer, ela deve consumir menos caloria do que ela necessita, para queimar a massa gorda. Agora, usando termos de nutrição fica mais fácil entender, vejamos: supondo que um indivíduo necessite de <b>2000kcal por dia</b>. Caso esse indivíduo consuma <b>2500kcal por dia </b>, ele irá ganhar massa gorda, pois estará consumindo mais do que está gastando. Se ele consumir <b>1500kcal por dia</b>, estará perdendo massa gorda, ou seja, <b>emagrecendo</b>. 
+
+			</p>
+
+			<div class="destaque">
+				<p>
+				
+				"Para uma pessoa emagrecer, ela deve consumir menos caloria do que ela necessita, para queimar a massa gorda."
+				</p>
+
+			</div>
+
+			<h2> Conclusão: </h2>
+
+			<p>
+
+			<p> 
+
+			&nbsp "Hm, então indivíduo que necessita de 2000kcal diárias, consumir 2500kcal, engordará para sempre?"
+
+			</p> 
+
+			<p>
+
+			&nbsp &nbsp Bom, a resposta é não. Mas por que não? Simples, porque a necessidade calórica está ligada ao peso. Quanto mais peso, mais calorias são necessárias para manter o peso. É por esse motivo que pessoas altas geralmente comem mais, pois elas tem mais massa. Aqui podemos observar uma das fórmulas utilizadas para calcular a taxa metabólica basal de indivíduos do sexo masculino.
+
+			</p>
+
+			<div class="tabela">
+
+			<p id="tbm"> TMB </p> 
+
+			<p> 66,5 + ( 13,75. P ) + ( 5,003. A ) - ( 6,755. I ). </p>
+
+			<p>	P = Peso em Kg; </p> 
+			<p> A = Altura em cm; </p>
+			<p> I = idade em anos </p>
+
+			</div>
+
+
+			<p>
+
+			&nbsp &nbsp Ao olharmos a fórmula, podemos concluir que quanto mais peso e altura a pessoa tem, maior será a <i>TMB</i> dela. Importante destacar que esse é apenas um dos fatores utilizados para calcular seu gasto energético total, então peço que não se baseiem apenas nisso para montar uma dieta. O correto é que o profissional nutricionista avalie o que é necessário e monte a dieta se adequando aos seus dados.
+
+			</p>
+
+	</section>
+
+	<footer class="rodape"> 
+
+		<h1 id="a">  </h1>
+		<p id="b"> Itallo e Brenno </p>
+
+	</footer>
+
+</body>
+</html>
