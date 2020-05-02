ffe00f62ce551db522d5cb1d9a777d11e7686bba
Itallo Vidal <48143354+ividalll666@users.noreply.github.com>
2020-05-02 17:14:42 -0300

    Add files via upload

---
+++ dietadetox.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="dietadetoxstyle.css">
+
+	<title> Nutricionando </title>
+</head>
+
+<body class="interface">
+
+
+	<figure class="foto-inicial">
+		<img src="_imagens/detoox.jpg">
+	</figure>
+
+	<nav>
+		<ul class="menu">
+			<li> Inicio</li>
+			<li> Matérias</li>
+			<li> Sobre nós </li>
+
+
+		</ul>
+	</nav>
+ 
+
+
+	<section class="conteudo">
+		
+			<h1> A dieta Detox </h1>
+
+			<h2> Desmistificando.. </h2>
+
+			<p>
+                &nbsp &nbsp Você certamente já ouvir falar em <b>"Fazer uma dieta detox"</b>. Hoje iremos falar sobre o assunto e o porquê a maioria dos pensamentos ao redor dessa dieta estão errados.
+                Tenho uma notícia para você: <i> não existe embasamento científico que dê valor a essas dietas </i>. Mas não se engane, o processo de desintoxificar o corpo realmente existe. 
+                A ingestão de compostos bioativos com regularidade faz com que haja a diminuição de compostos intoxicantes no organismo. Não se esqueça também de diminuir a quantidade de 
+                comidas industrializadas que você ingere, e opte por opções com menos aditivos químicos. Caso contrário, estará enxugando gelo.
+
+
+			</p>
+
+			
+			<div class="destaque">
+				<p>
+
+				"Não existe embasamento científico que dê valor a essas dietas"
+
+				</p>
+
+			</div>
+
+			<h2> O que são compostos bioativos? </h2>
+
+			<p>	
+
+            &nbsp &nbsp São compostos químicos que, diferente dos macro e micronutrientes, não exercem função vital no organismo, mas contribuem para um melhor funcionamento dos tecidos e 
+            para uma melhor qualidade da pele, dos cabelos, unhas e fatores anti envelhecimento. Mas para isso funcionar é necessário que todos os nutrientes estejam em quantidade
+            ótima, ou seja, na quantidade ideal recomendada por dia. O que adiantaria consumir antioxidantes se sua dieta está desbalanceada? É importante enxergar a dieta como o pilar central, 
+            e os compostos bioativos como alicerces que contribuem para melhorar a sustentação.  
+
+			</p>
+
+			<div class="destaque">
+				<p>
+                    O que adiantaria consumir antioxidantes se sua dieta está desbalanceada?
+				</p>
+
+			</div>
+
+			<h2> Qual a diferença de um composto intoxicante e uma toxina? </h2>
+
+			<p> 
+
+            &nbsp &nbsp Composto intoxicante é aquele que não possui função biológica. Por exemplo, o alumínio. O alumínio é um metal pesado, entretanto ele está ao nosso redor em panelas, desodorantes, caixas, etc...
+            O mercúrio é outro tipo de metal pesado, muito presente em peixes, principalmente os peixes que se encontram em uma posição elevada na cadeia alimentar, pois estes comem outros peixes, aumentando sua 
+            concentração total de mercúrio.
+
+            </p> 
+            
+            <div class="destaque">
+				<p>
+
+				"Composto intoxicante é aquele que não possui função biológica."
+
+				</p>
+
+            </div>
+            
+			<p>
+
+            &nbsp &nbsp Enquanto toxinas, são compostos biológicos, principalmente proteínas, que causam danos ao organismo, geralmente ligando-se a receptores nas células e bloqueando função enzimática.  
+            Uma das toxinas mais famosas e perigosas que existem no ramo da nutrição é a toxina botulínica, a mesma utilizada na aplicação do ''botox''. Essa toxina entre em contato com a corrente sanguínea e, mesmo em 
+            quantidades ínfimas, é capaz de causar paralisia da musculatura respiratória.
+
+            </p>
+            
+            <div class="destaque">
+				<p>
+
+				"Enquanto toxinas, são compostos biológicos(...)"
+
+				</p>
+
+            </div>
+            
+            <p> &nbsp &nbsp Espero que eu tenha trazido conhecimento e que tenha gostado da matéria! </p>
+
+
+	</section>
+
+	<footer class="rodape"> 
+
+		<h1 id="a">  </h1>
+		<p id="b"> Itallo e Brenno </p>
+
+	</footer>
+
+</body>
+</html>
